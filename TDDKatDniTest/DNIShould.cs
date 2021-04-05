f621f3f2a57f9f54bd17ba4362534d0e90ad2f97
mariojespada <mario.jimenez@webbeds.com>
2021-04-05 19:46:17 +0200

    checked dni

---
+++ TDDKatDniTest/DNIShould.cs
@@ -0,0 +1,48 @@
+using System;
+using TDDKataDNI;
+using Xunit;
+
+namespace TDDKatDniTest
+{
+    public class DNIShould
+    {
+        [Theory]
+        [InlineData ("1234567890R")]
+        [InlineData("12345678R")]
+        public void Have_no_other_than_10_digits(string dniNumber)
+        {
+            Assert.Throws<ArgumentException>(() => new Dni(dniNumber));
+        }
+
+        [Fact]
+        public void Have_10_digits()
+        {
+            var dni = new Dni("123456789R");
+            Assert.NotNull(dni);
+        }
+
+        [Fact]
+        public void First_9_must_be_numbers()
+        {
+            Assert.Throws<ArgumentException>(() => new Dni("12345678R9"));
+        }
+
+        [Fact]
+        public void Last_must_be_letter()
+        {
+            Assert.Throws<ArgumentException>(() => new Dni("1234567890"));
+        }
+
+        [Theory]
+        [InlineData("123456789Ñ")]
+        [InlineData("123456789U")]
+        [InlineData("123456789O")]
+        [InlineData("123456789I")]
+        [InlineData("123456789i")]
+        public void Is_valid_letter(string dniNumber)
+        {
+            Assert.Throws<ArgumentException>(() => new Dni(dniNumber));
+        }
+
+    }
+}
--- TDDKataDNI/Dni.cs
+++ TDDKataDNI/Dni.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TDDKataDNI
 {
     public record Dni
     {
+        public readonly char[] invalidLetters = new char[] { 'U', 'I', 'O', 'Ñ' };
+
         public Dni(string dniNumber)
         {
-            throw new ArgumentException();
+            EnsureDNIIsTenCharactersLong(dniNumber);            
+            EnsureLastCharacterIsALetter(dniNumber);
+            EnsureLetterIsCorrect(dniNumber);
+            EnsureFirstNineCharactersAreNumbers(dniNumber);
+        }
+
+        private void EnsureLetterIsCorrect(string dniNumber)
+        {
+            var lastCharacter = dniNumber.Last();
+            var upperCaseLetter = char.ToUpper(lastCharacter);
+            if (invalidLetters.Contains(upperCaseLetter))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        private void EnsureFirstNineCharactersAreNumbers(string dniNumber)
+        {
+            var dniNumberPart = dniNumber.Take(9);
+
+            if (!dniNumberPart.All(x => char.IsDigit(x)))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        private void EnsureDNIIsTenCharactersLong(string dniNumber)
+        {
+            if (dniNumber.Length != 10)
+            {
+                throw new ArgumentException();
+            }
+        }
+               
+        private void EnsureLastCharacterIsALetter(string dniNumber)
+        {
+            var lastCharacter = dniNumber.Last();
+            if(!char.IsLetter(lastCharacter))
+            {
+                throw new ArgumentException();
+            }
         }
     }
 }
